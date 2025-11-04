13e1d22e0662ac4a8ed2d44fec06e874c1cc77d7
lordatroz <esteb.nofx182@hotmail.com>
2025-11-04 09:21:21 -0500

    Actualizar complements.js

--- static/complements.js
+++ static/complements.js
@@ -1,5 +1,4 @@
-// Si necesitas lógica del lado del cliente, déjala aquí.
-// Por ejemplo, copiar al portapapeles:
+
 document.addEventListener("click", (e) => {
   if (e.target.matches("[data-copy]")) {
     navigator.clipboard.writeText(e.target.dataset.copy || "");
